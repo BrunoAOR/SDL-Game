e7eb7939b1c84ef4362a4b71ed31b352f0b0ecbc
Bruno Ortiz <brunoorla@gmail.com>
2017-09-20 15:57:31 +0200

    CHANGED: Improved the Vector2 class

--- SDL Game/SDL Game/Engine/Vector2.cpp
+++ SDL Game/SDL Game/Engine/Vector2.cpp
@@ -10,19 +10,105 @@ Vector2::Vector2(double x, double y) : x(x), y(y)
 {
 }
 
-double Vector2::getLength()
+double Vector2::getLength() const
 {
 	return sqrt(getLengthSquared());
 }
 
 
-double Vector2::getLengthSquared()
+double Vector2::getLengthSquared() const
 {
 	return x * x + y * y;
 }
 
+Vector2 Vector2::normalized() const
+{
+	double length = getLength();
+	if (length == 0)
+	{
+		return *this;
+	}
+	return *this / length;
+}
+
+void Vector2::normalize()
+{
+	double length = getLength();
+	if (length == 0)
+	{
+		return;
+	}
+	x /= length;
+	y /= length;
+}
+
+Vector2 & Vector2::operator+=(const Vector2 & rhs)
+{
+	this->x += rhs.x;
+	this->y += rhs.y;
+	return *this;
+}
+
+Vector2 & Vector2::operator-=(const Vector2 & rhs)
+{
+	this->x -= x;
+	this->y -= y;
+	return *this;
+}
+
+Vector2 & Vector2::operator*=(double num)
+{
+	this->x *= num;
+	this->y *= num;
+	return *this;
+}
+
+Vector2 & Vector2::operator/=(double num)
+{
+	this->x /= num;
+	this->y /= num;
+	return *this;
+}
+
+double Vector2::distance(const Vector2 & lhs, const Vector2 & rhs)
+{
+	return Vector2(lhs.x - rhs.x, lhs.y - rhs.y).getLength();
+}
+
+
+Vector2 operator-(const Vector2 & v2)
+{
+	return Vector2(-v2.x, -v2.y);
+}
 
 Vector2 operator+(Vector2 lhs, const Vector2 & rhs)
 {
 	return Vector2(lhs.x + rhs.x, lhs.y + rhs.y);
 }
+
+Vector2 operator-(Vector2 lhs, const Vector2 & rhs)
+{
+	return Vector2(lhs.x - rhs.x, lhs.y - rhs.y);
+}
+
+Vector2 operator*(double num, const Vector2 & v2)
+{
+	return Vector2(v2.x * num, v2.y * num);
+}
+
+Vector2 operator/(double num, const Vector2 & v2)
+{
+	return Vector2(v2.x / num, v2.y / num);
+}
+
+Vector2 operator*(Vector2 v2, double num)
+{
+	return operator*(num, v2);
+}
+
+Vector2 operator/(Vector2 v2, double num)
+{
+	return operator/(num, v2);
+}
+
+
--- SDL Game/SDL Game/Engine/Vector2.h
+++ SDL Game/SDL Game/Engine/Vector2.h
@@ -7,11 +7,27 @@ public:
 	Vector2();
 	Vector2(double x, double y);
 
-	double getLength();
-	double getLengthSquared();
+	double getLength() const;
+	double getLengthSquared() const;
+	Vector2 normalized() const;
+	void normalize();
 
+	friend Vector2 operator- (const Vector2& v2);
 	friend Vector2 operator+ (Vector2 lhs, const Vector2& rhs);
+	friend Vector2 operator- (Vector2 lhs, const Vector2& rhs);
+	friend Vector2 operator* (double num, const Vector2& v2);
+	friend Vector2 operator/ (double num, const Vector2& v2);
+	friend Vector2 operator* (Vector2 v2, double num);
+	friend Vector2 operator/ (Vector2 v2, double num);
+	
+
+	Vector2& operator+= (const Vector2& rhs);
+	Vector2& operator-= (const Vector2& rhs);
+	Vector2& operator*= (double num);
+	Vector2& operator/= (double num);
 
 	double x;
 	double y;
+
+	static double distance(const Vector2& lhs, const Vector2& rhs);
 };
